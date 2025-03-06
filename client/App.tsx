46996de25e553d7e906b609e0ad630c173e3c7d4
Ricardo Puma <103985497+ricnrdo@users.noreply.github.com>
2025-03-06 15:05:54 -0500

    front-end done

--- client/App.tsx
+++ client/App.tsx
@@ -4,14 +4,16 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './app/screens/login';
+import Signup from './app/screens/login';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="login" component={Login} options={{ headerShown: false }} />
+      <Stack.Navigator initialRouteName='Login'>
+        <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+        <Stack.Screen name="Signup" component={Signup} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- client/app/screens/login.tsx
+++ client/app/screens/login.tsx
@@ -1,52 +1,93 @@
 import { StatusBar } from 'expo-status-bar';
 import React from "react";
-import { Text, TextInput, View, SafeAreaView, StyleSheet, Image, Button, Alert } from "react-native";
+import { Text, TextInput, View, SafeAreaView, StyleSheet, Keyboard, TouchableWithoutFeedback, Alert, Dimensions } from "react-native";
 import Logo from "../assets/logo.svg";
 
 export default function Login() {
-    const [user, onChangeUsername] = React.useState('')
-    const [pass, onChangePassword] = React.useState('')
+    const [user, onChangeUsername] = React.useState('');
+    const [pass, onChangePassword] = React.useState('');
+
+    const {width} = Dimensions.get('screen');
 
     return (
-        <View style={styles.container}>
-            <Logo width={100} height={100} />
-            <Text>Login</Text>
-            <TextInput
-              style={styles.input}
-              onChangeText={onChangeUsername}
-              value={user}
-              placeholder="Username"
-            />
-            <TextInput
-              style={styles.input}
-              onChangeText={onChangePassword}
-              value={pass}
-              placeholder="Password"
-            />
-            <Button
-              title="Submit"
-              onPress={() => Alert.alert(user,pass)}
-            />
-        </View>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <SafeAreaView style={styles.safeArea}>
+                <View style={styles.container}>
+                    {/* Logo Section */}
+                    <View style={styles.logoContainer}>
+                        <Logo width={width * 0.5}/>
+                    </View>
+                    {/* Form Section */}
+                    <View style={[styles.formContainer, {width: width * 0.75, maxWidth: 400}]}>
+                        <TextInput
+                        style={styles.input}
+                        onChangeText={onChangeUsername}
+                        value={user}
+                        placeholder="Username"
+                        />
+                        <TextInput
+                        style={styles.input}
+                        onChangeText={onChangePassword}
+                        value={pass}
+                        placeholder="Password"
+                        />
+                        <TouchableWithoutFeedback>
+                            <Text style={{color: "#b0aaaa", textAlign: "left", textDecorationLine: "underline",}} onPress={() => Alert.alert("Reset Password")}>
+                                Forgot Your Password?
+                            </Text>
+                        </TouchableWithoutFeedback>
+                        <TouchableWithoutFeedback>
+                            <Text style={styles.button} onPress={() => Alert.alert("Log In")}>Log In</Text>
+                        </TouchableWithoutFeedback>
+                        <View style={{marginTop: 20}}>
+                            <Text>Don't have an account? <Text style={{color: "#1D3B2A", textDecorationLine: "underline"}} onPress={() => Alert.alert("Sign Up")}>Sign Up</Text>
+                            </Text>
+                        </View>
+                    </View>
+                </View>
+            </SafeAreaView>
+        </TouchableWithoutFeedback>
     );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  logo : {
-    width: 200,
-    height: 200,
-    resizeMode: 'contain',
-  },
-  input : {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-  }
+    safeArea: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    container: {
+        flex: 1,
+        width: '100%',
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: 25,
+    },
+    logoContainer : {
+        backgroundColor: '#00000',
+        paddingBottom: 75,
+        top: "20%",
+        position: 'absolute',
+    },
+    formContainer : {
+        backgroundColor: '#00000',
+        paddingTop: 75,
+    },
+    input : {
+        height: 50,
+        borderWidth: 1,
+        padding: 10,
+        borderRadius: 10,
+        borderColor: '#c2',
+        flexDirection: 'row',
+        marginBottom: 20,
+    },
+    button: {
+        backgroundColor: '#1D3B2A',
+        color: '#fff',
+        padding: 12,
+        borderRadius: 10,
+        textAlign: 'center',
+        marginTop: 20,
+    }
 });
